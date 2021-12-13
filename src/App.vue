bb9bb31c82aeaab1de662908aef882141c044e52
Ewelina <ewelinazwolenik421@gmail.com>
2021-12-13 23:04:56 +0100

    removed useless sections

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <ReservationInfo />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import ReservationInfo from './components/ReservationInfo.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    ReservationInfo,
   },
 })
 export default class App extends Vue {}
